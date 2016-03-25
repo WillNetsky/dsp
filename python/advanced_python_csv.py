d0eeb2ed1063e1b5885d1877c18a5c266c3e7882
Will Chernetsky <willnetsky@gmail.com>
2016-03-24 21:54:46 -0700

    completed csv section

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,11 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import sys
+
+
+def main(argv):
+    faculty = pd.read_csv('faculty.csv')
+    faculty['email'].to_csv('emails.csv', index=False)
+
+if __name__ == '__main__':
+    main(sys.argv)
+
